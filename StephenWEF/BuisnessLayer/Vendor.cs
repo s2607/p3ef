9ac34ea8cb4057197f9dc8e0064b143e20ca5f32
s2607 <s2670@email.vccs.edu>
2015-04-13 01:11:22 -0700

    missing files

---
+++ StephenWEF/BuisnessLayer/Vendor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkInventory
+{
+	public partial class Vendor
+	{
+		public Decimal TotalPurchases
+		{
+			get
+			{
+				//Returns a decimal of the total purchases for the Vendor as a public readonly property.
+				return PurchaseOrders.Sum(po => po.PurchaseOrderParts.Sum(pop => pop.ExtendedCost));
+			}
+		}
+		public List<Part> GetAllParts
+		{
+			get
+			{
+				//Returns a List<Part> of all parts ordered from the Vendor as a read-only public property. 
+
+				return (from po in PurchaseOrders
+						from pop in po.PurchaseOrderParts
+						select pop.Part).ToList();
+			}
+		}
+	}
+}
---
+++ StephenWEF/BuisnessLayer/part.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityFrameworkInventory; //NOTE: this isnt added automaticly
+using EntityFrameworkInventory.Repository;
+
+namespace EntityFrameworkInventory
+{
+	public partial class Part
+	{
+		public static List<Part> GetPartsWithoutSpoilages()
+		{
+			var paRep = new PartRepository();
+			var parts = paRep.All().ToList();
+			return parts.Where(p => p.Spoilages.Count < 1).ToList();
+		}
+		public static Part GetHighestValue()
+		{
+			//Static method that returns the Part with the highest current inventory value as a public static method. 
+			var paRep = new PartRepository();
+			var parts = paRep.All().ToList();
+			return parts.OrderByDescending(p => p.CurrentValue).FirstOrDefault();
+
+		}
+		public static Dictionary<int,Part> GetPartDictionary(){
+			//Static method that returns the Part with the highest current inventory value as a public static method. 
+			var paRep = new PartRepository();
+			var parts = paRep.All().ToList();
+			//return (from part in parts group part by part.PartId into pg select pg).ToDictionary();
+			return parts.ToDictionary(p => p.PartId);
+
+		}
+		public static List<Part> GetAllParts( PurchaseOrder PurchaseOrder ) {	
+			//A public static method that returns a List<Part> of Parts for a given PurchaseOrder parameter.
+			return (from pop in PurchaseOrder.PurchaseOrderParts select pop.Part).ToList();
+		}
+
+	}
+
+}
